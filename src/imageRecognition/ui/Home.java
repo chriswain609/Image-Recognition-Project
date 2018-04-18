dc7dcb94965172e093701eecfbff5df91a614c5b
chriswain609 <38350680+chriswain609@users.noreply.github.com>
2018-04-18 11:28:36 +0100

    Add files via upload

---
+++ src/imageRecognition/ui/Home.java
@@ -0,0 +1,66 @@
+package imageRecognition.ui;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class Home extends JFrame {
+
+	private JPanel contentPane;
+	
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Home frame = new Home();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Home() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton btnAdmin = new JButton("Admin");
+		btnAdmin.setBounds(78, 178, 146, 32);
+		contentPane.add(btnAdmin);
+		
+		JButton btnImageRecognition = new JButton("Image Recognition");
+		btnImageRecognition.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//try {
+					UploadImage imageUp = new UploadImage();
+					imageUp.setVisible(true);
+					frame.dispose();
+			//	}catch(Exception e1) {
+					//JOptionPane.showMessageDialog(null, e1);
+				//}
+			}
+		});
+		btnImageRecognition.setBounds(244, 178, 146, 32);
+		contentPane.add(btnImageRecognition);
+	}
+}
