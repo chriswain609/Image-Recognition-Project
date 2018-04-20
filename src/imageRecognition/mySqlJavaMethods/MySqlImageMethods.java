981c3011bdb89fa661074ad43cdabb211974e02e
Alex <der@star73.com>
2018-04-20 15:47:01 +0100

    Update MySqlImageMethods.java

--- src/imageRecognition/mySqlJavaMethods/MySqlImageMethods.java
+++ src/imageRecognition/mySqlJavaMethods/MySqlImageMethods.java
@@ -5,39 +5,47 @@ import java.io.*;
 
 public class MySqlImageMethods 
 {
-	public MySqlImageMethods 
+	private String driverName;
+	private String url;
+	private String dbName;
+	private String userName;
+	private String password;
+	private Connection con;
+	
+	
+	public MySqlImageMethods()
 	{
-		String driverName = "com.mysql.jdbc.Driver";
-		String url = "jdbc:mysql://mysql.csc.liv.ac.uk:3306/";
-		String dbName = "u5af";
-		String userName = "u5af";
-		String password = "";
-		Connection con = null;
+		driverName = "com.mysql.jdbc.Driver";
+		url = "jdbc:mysql://mysql.csc.liv.ac.uk:3306/";
+		dbName = "u5af";
+		userName = "u5af";
+		password = "";
+		con = null;
 	}
 	
 	public void InsertImage()
 	{
 		try{
-		   Class.forName(driverName);
-		   con = DriverManager.getConnection(url+dbName,userName,password);
-		   Statement st = con.createStatement();
-		   File imgfile = new File("H:/Pictures/maxresdefault.jpg");
+			Class.forName(driverName);
+			con = DriverManager.getConnection(url+dbName,userName,password);
+			Statement st = con.createStatement();
+			File imgfile = new File("H:/Pictures/maxresdefault.jpg");
 
-		  FileInputStream fin = new FileInputStream(imgfile);
+			FileInputStream fin = new FileInputStream(imgfile);
 
-		   PreparedStatement pre =
-		   con.prepareStatement("insert into images values(?,?,?,?,?)");
+			PreparedStatement pre =
+			con.prepareStatement("insert into images values(?,?,?,?,?)");
 
-		   pre.setString(1,"789");
-		   pre.setInt(2,3);
-		   pre.setBinaryStream(3,(InputStream)fin,(int)imgfile.length());
-		   pre.setInt(4,69);
-		   pre.setString(5,"Y");
-		   pre.executeUpdate();
-		   System.out.println("Successfully inserted the file into the database!");
+			pre.setString(1,"369");
+			pre.setInt(2,3);
+			pre.setBinaryStream(3,(InputStream)fin,(int)imgfile.length());
+			pre.setInt(4,69);
+			pre.setString(5,"Y");
+			pre.executeUpdate();
+			System.out.println("Successfully inserted the file into the database!");
 
-		   pre.close();
-		   con.close(); 
+			pre.close();
+			con.close(); 
 		}catch (Exception e1){
 			System.out.println(e1.getMessage());
 		}
