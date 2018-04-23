2480a440ab9ab27afe9e20cf8ed412bccea4a2a0
chriswain609 <38350680+chriswain609@users.noreply.github.com>
2018-04-23 13:42:43 +0100

    Create ImageView.java

---
+++ src/imageRecognition/ui/ImageView.java
@@ -0,0 +1,63 @@
+package imageRecognition.ui;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.Border;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class ImageView extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ImageView frame = new ImageView();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public ImageView() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 545, 430);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblImage = new JLabel("");
+		lblImage.setBounds(30, 24, 470, 326);
+		Border border = BorderFactory.createLineBorder(Color.BLACK, 5);
+		lblImage.setBorder(border);
+		contentPane.add(lblImage);
+		
+		JButton btnBack = new JButton("Back");
+		btnBack.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnBack.setBounds(209, 362, 117, 29);
+		contentPane.add(btnBack);
+	}
+
+}
