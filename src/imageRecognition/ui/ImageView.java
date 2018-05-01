8ef4bd3344092450d8d39deb6aa04edbe13b3c27
Alex <der@star73.com>
2018-05-01 17:03:45 +0100

    Update ImageView.java

--- src/imageRecognition/ui/ImageView.java
+++ src/imageRecognition/ui/ImageView.java
@@ -3,20 +3,33 @@ package imageRecognition.ui;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.EventQueue;
+import java.awt.Image;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.Border;
 import javax.swing.border.EmptyBorder;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import javax.swing.JLabel;
+import javax.imageio.ImageIO;
 import javax.swing.BorderFactory;
+import javax.swing.Icon;
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.ImageIcon;
+
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.awt.event.ActionEvent;
 
 public class ImageView extends JFrame {
 
 	private JPanel contentPane;
+	private JLabel lblImage;
 
 	/**
 	 * Launch the application.
@@ -25,8 +38,7 @@ public class ImageView extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					ImageView frame = new ImageView();
-					frame.setVisible(true);
+					new ImageView();
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -38,6 +50,7 @@ public class ImageView extends JFrame {
 	 * Create the frame.
 	 */
 	public ImageView() {
+		setTitle("View Image");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 545, 430);
 		contentPane = new JPanel();
@@ -45,8 +58,15 @@ public class ImageView extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JLabel lblImage = new JLabel("");
+		lblImage = new JLabel("");
 		lblImage.setBounds(30, 24, 470, 326);
+		BufferedImage picture = null;
+		try {
+			picture = ImageIO.read(new File("tempImageFile.jpg"));
+		} catch (IOException ioe) {
+			ioe.printStackTrace();
+		}
+		lblImage.setIcon(ResizeImage(picture));
 		Border border = BorderFactory.createLineBorder(Color.BLACK, 5);
 		lblImage.setBorder(border);
 		contentPane.add(lblImage);
@@ -54,10 +74,22 @@ public class ImageView extends JFrame {
 		JButton btnBack = new JButton("Back");
 		btnBack.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				try {
+					Files.deleteIfExists(Paths.get("H:/eclipse17/imageRec/tempImageFile.jpg"));
+				} catch( IOException ioe) {
+					ioe.printStackTrace();
+				}
+				AdminPage.getViewer().dispose();
 			}
 		});
 		btnBack.setBounds(209, 362, 117, 29);
 		contentPane.add(btnBack);
+	}
+	
+	public ImageIcon ResizeImage(BufferedImage pic) {
+		Image newImg = pic.getScaledInstance(lblImage.getWidth(), lblImage.getHeight(), Image.SCALE_SMOOTH);
+		ImageIcon image = new ImageIcon(newImg);
+		return image;
 	}
 
 }
