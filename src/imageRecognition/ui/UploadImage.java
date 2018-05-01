3cd6258884fe98c405ccb9709fdf05e8fa1fd08b
Alex <der@star73.com>
2018-05-01 17:04:49 +0100

    Update UploadImage.java

--- src/imageRecognition/ui/UploadImage.java
+++ src/imageRecognition/ui/UploadImage.java
@@ -11,6 +11,7 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.border.Border;
 import javax.swing.border.EmptyBorder;
+import javax.imageio.ImageIO;
 import javax.swing.BorderFactory;
 import javax.swing.ButtonGroup;
 import javax.swing.ImageIcon;
@@ -18,7 +19,15 @@ import javax.swing.JButton;
 import javax.swing.JFileChooser;
 
 import java.awt.event.ActionListener;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.awt.event.ActionEvent;
 import javax.swing.filechooser.FileNameExtensionFilter;
 import javax.swing.JComboBox;
@@ -29,6 +38,9 @@ public class UploadImage extends JFrame {
 	private JPanel contentPane;
 	private boolean analysed = false;
 	private boolean imageChosen = false;
+	private File selectedFile;
+	private JRadioButton rdbtnYes;
+	private JRadioButton rdbtnNo;
 
 	/**
 	 * Launch the application.
@@ -69,10 +81,10 @@ public class UploadImage extends JFrame {
 				JFileChooser file = new JFileChooser();
 				file.setCurrentDirectory(new File(System.getProperty("user.home")));
 				FileNameExtensionFilter filter = new FileNameExtensionFilter("*.Images','jpg','gif","png");
-				file.addChoosableFileFilter(filter);;
+				file.addChoosableFileFilter(filter);
 				int result = file.showSaveDialog(null);
 				if(result == JFileChooser.APPROVE_OPTION) {
-					File selectedFile = file.getSelectedFile();
+					selectedFile = file.getSelectedFile();
 					String path = selectedFile.getAbsolutePath();
 					lblImage.setIcon(ResizeImage(path));
 					analysed = false;
@@ -113,11 +125,11 @@ public class UploadImage extends JFrame {
 		lblIsThisCorrect.setBounds(425, 204, 98, 16);
 		contentPane.add(lblIsThisCorrect);
 		
-		JRadioButton rdbtnYes = new JRadioButton("Yes");
+		rdbtnYes = new JRadioButton("Yes");
 		rdbtnYes.setBounds(447, 250, 141, 23);
 		contentPane.add(rdbtnYes);
 		
-		JRadioButton rdbtnNo = new JRadioButton("No");
+		rdbtnNo = new JRadioButton("No");
 		rdbtnNo.setBounds(448, 285, 141, 23);
 		contentPane.add(rdbtnNo);
 		
@@ -137,6 +149,37 @@ public class UploadImage extends JFrame {
 		JButton btnSubmit = new JButton("Submit");
 		btnSubmit.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				try {
+					Class.forName("com.mysql.jdbc.Driver").newInstance();
+					Connection con = DriverManager.getConnection(
+							"jdbc:mysql://mysql.csc.liv.ac.uk:3306/u5af", "u5af", "");
+			        
+					
+					File imgfile = new File(selectedFile.getPath());
+
+					FileInputStream fin = new FileInputStream(imgfile);
+
+					PreparedStatement pre =
+					con.prepareStatement("INSERT INTO images VALUES(?,?,?,?,?)");
+
+					pre.setString(1, setLabelName("1"));
+					
+					pre.setString(2, lblImageGuess.getText());
+					
+					pre.setBinaryStream(3,(InputStream)fin,(int)imgfile.length());
+					
+					pre.setInt(4, Integer.parseInt(lblConfidenceOutput.getText()));
+					
+					pre.setString(5,checkGuess());
+					
+					pre.executeUpdate();
+					System.out.println("Successfully inserted the file into the database!");
+
+					pre.close();
+					con.close(); 
+				}catch (Exception e1){
+					e1.printStackTrace();
+				}
 				if(!imageChosen) {
 					JOptionPane.showMessageDialog(null, "You must choose an image and analyse it first");
 				}
@@ -165,5 +208,40 @@ public class UploadImage extends JFrame {
 		});
 		btnBack.setBounds(26, 12, 98, 23);
 		contentPane.add(btnBack);
+	}
+	
+	public String setLabelName(String label) {
+		String labelCheck = "";
+		try {
+			Class.forName("com.mysql.jdbc.Driver").newInstance();
+			Connection con = DriverManager.getConnection(
+					"jdbc:mysql://mysql.csc.liv.ac.uk:3306/u5af", "u5af", "");
+			
+			Statement statement = con.createStatement();
+            ResultSet resultSet;
+            resultSet = statement.executeQuery("SELECT label FROM images WHERE label = '"+label.trim()+"'");
+            while (resultSet.next()) {
+            	labelCheck = resultSet.getString(1);
+            }
+            if (labelCheck.equals(label)) {
+            	int change = Integer.parseInt(label);
+            	change++;
+            	label = Integer.toString(change);
+            	setLabelName(label);
+            }
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+		return label;
+	}
+	
+	public String checkGuess() {
+		String answer = "";
+		if (rdbtnYes.isSelected() == true) {
+			answer = "Y";
+		} else if (rdbtnNo.isSelected() == true) {
+			answer = "N";
+		}
+		return answer;
 	}
 }
