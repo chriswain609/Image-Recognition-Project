7f81418d1a87521eac6cc4a3079e28d3f0a24012
Alex <der@star73.com>
2018-05-03 15:55:02 +0100

    Added serialVersion for eclipse

--- src/imageRecognition/ui/Home.java
+++ src/imageRecognition/ui/Home.java
@@ -1,20 +1,23 @@
 package imageRecognition.ui;
 
-import java.awt.BorderLayout;
+
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
-import javax.swing.JTextField;
 import javax.swing.JLabel;
 import java.awt.Font;
 
 public class Home extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 
 	private JPanel contentPane;
 	
